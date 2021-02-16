b056cb5ad36b2a86e1878e3a8d291e2c9c890219
Farhan Apriliandi <apriliandi642@gmail.com>
2021-02-17 03:17:52 +0700

    add mobile version (dev.to)

---
+++ dev.to/js/script.js
@@ -0,0 +1,6 @@
+const asideLeft = document.querySelector(".aside-left");
+const toggleMenu = document.querySelector(".toggle-menu");
+
+toggleMenu.addEventListener("click", () => {
+   asideLeft.style.display = "block";
+});
