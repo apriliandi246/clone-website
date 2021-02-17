015ef998e8e1f40d711850eebc005b9573851787
Farhan Apriliandi <apriliandi642@gmail.com>
2021-02-18 01:36:50 +0700

    add some improvement for responsive

--- dev.to/js/script.js
+++ dev.to/js/script.js
@@ -4,3 +4,15 @@ const toggleMenu = document.querySelector(".toggle-menu");
 toggleMenu.addEventListener("click", () => {
    asideLeft.style.display = "block";
 });
+
+document.querySelector(".join__close-button").addEventListener("click", () => {
+   asideLeft.style.display = "none";
+});
+
+window.addEventListener("resize", () => {
+   if (window.innerWidth >= 1020) {
+      asideLeft.style.display = "block";
+   } else {
+      asideLeft.style.display = "none";
+   }
+});
